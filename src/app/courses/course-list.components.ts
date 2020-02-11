2ac6809827144bd0dba12d4dbbe5a0f75ebcc4a4
thiago <tvttavares@gmail.com>
2020-02-10 23:06:42 -0400

    Dealing with Dependency Injection

--- src/app/courses/course-list.components.ts
+++ src/app/courses/course-list.components.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component ({
     selector: 'app-course-list',
@@ -8,29 +9,12 @@ import { Course } from './course';
 export class CourseListComponent implements OnInit {
 
     courses: Course[] = [];
-    ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-8796',
-                duration: 120,
-                rating: 4.5, 
-                releaseDate: 'December, 2, 2019'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 45.99,
-                code: 'LPL-1468',
-                duration: 80,
-                rating: 5,
-                releaseDate: 'March, 4, 2019'
-            }
-        ]
 
+    constructor(private courseService: CourseService) {
+
+    }
+
+    ngOnInit(): void {
+        this.courses = this.courseService.retrieveAll();
     }
 }
