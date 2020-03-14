0d9535b924816b4fa9234a77bf92d9f203727b59
thiago <tvttavares@gmail.com>
2020-03-14 12:13:03 -0400

    Segregating the application into modules

---
+++ src/app/courses/course.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CourseListComponent } from './course-list.components';
+import { CourseInfoComponent } from './course-info.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ReplacePipe } from '../pipe/replace.pipe';
+import { CommonModule } from '@angular/common';
+import { StarComponent } from '../star/star.component';
+
+@NgModule({
+    declarations: [
+        CourseListComponent,
+        CourseInfoComponent,
+        ReplacePipe,
+        StarComponent
+    ], imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild([
+            {
+                path: 'courses', component: CourseListComponent
+              },
+              {
+                path: 'courses/info/:id', component: CourseInfoComponent
+              }
+        ])
+    ]
+})
+export class CourseModule {
+
+}
