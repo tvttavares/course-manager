c7a59e80c6bf5d17fadc10b54470e74a9d3ee18c
thiago <tvttavares@gmail.com>
2020-03-14 12:32:12 -0400

    Knowing the shared folder

--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -1,21 +1,21 @@
+import { AppPipeModule } from './../shared/pipe/app-pipe.module';
 import { NgModule } from '@angular/core';
 import { CourseListComponent } from './course-list.components';
 import { CourseInfoComponent } from './course-info.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { ReplacePipe } from '../pipe/replace.pipe';
 import { CommonModule } from '@angular/common';
-import { StarComponent } from '../star/star.component';
+import { StarModule } from '../shared/component/star/star.module';
 
 @NgModule({
     declarations: [
         CourseListComponent,
-        CourseInfoComponent,
-        ReplacePipe,
-        StarComponent
+        CourseInfoComponent
     ], imports: [
         CommonModule,
         FormsModule,
+        StarModule,
+        AppPipeModule,
         RouterModule.forChild([
             {
                 path: 'courses', component: CourseListComponent
